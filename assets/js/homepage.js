3b9760ff266cc7597892fe640319207fd44299fa
maxkohn <kohn.max@gmail.com>
2021-10-31 18:57:49 -0400

    add username form and initial username logic handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector('#username');
+
 var getUserRepos = function(user) {
 
     //format the github api url
@@ -10,6 +13,20 @@ var getUserRepos = function(user) {
     });
 };
 
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.ariaValueMax.trim();
+
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value="";
+    } else {
+        alert("Please enter a Github Username");
+    };
+    console.log(event);
+};
 
 
-getUserRepos();
+
+
+userFormEl.addEventListener("submit",formSubmitHandler);
